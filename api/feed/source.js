6ef1ee9253bbb0438d504fece47dbaab134eb50a
tillsangs <tillsangs@gmail.com>
2025-11-11 16:26:36 +0100

    Update source.js

--- api/feed/source.js
+++ api/feed/source.js
@@ -2,38 +2,49 @@
 const episodes = require("../../content/episodes.json");
 
 module.exports = async (req, res) => {
-  // ---- Kanal-metadata (viktigt: iTunes author på kanalnivå) ----
+  // ---- Kanal-metadata ----
   const CHANNEL = {
     title: "Till Sängs (Källfeed)",
     link: "https://www.tillsangs.se/",
     language: "sv-SE",
     description:
       "Lyssna på Till Sängs i full längd utan reklam. Med Samanda Ekman & Malena Ivarsson. Medlemskap ger hela arkivet och alla nya avsnitt.",
-    itunesAuthor: "Samanda Ekman & Malena Ivarsson",
-    itunesExplicit: "false",
-    itunesImage: "https://www.tillsangs.se/cover.jpg",
-    itunesOwnerName: "Samanda Ekman",
-    itunesOwnerEmail: "info@tillsangs.com",
+    author: "Samanda Ekman & Malena Ivarsson",
+    explicit: "false",
+    image: "https://www.tillsangs.se/cover.jpg",
+    ownerName: "Samanda Ekman",
+    ownerEmail: "info@tillsangs.com",
   };
 
-  const items = episodes
-    .map(
-      (ep) => `
-    <item>
-      <title><![CDATA[${ep.title}]]></title>
-      <description><![CDATA[${ep.descriptionHtml}]]></description>
-      <pubDate>${new Date(ep.published).toUTCString()}</pubDate>
-      <guid isPermaLink="false">${ep.id}</guid>
-      <enclosure url="${ep.audio.private_mp3}" length="${ep.audio.bytes}" type="audio/mpeg" />
-      <itunes:duration>${ep.audio.duration}</itunes:duration>
-      <itunes:image href="${ep.image}" />
-    </item>`
-    )
+  // ---- Avsnitt ----
+  const items = (episodes || [])
+    .map((ep) => {
+      const title = ep.title || "Avsnitt";
+      const desc = ep.descriptionHtml || "";
+      const pub = ep.published ? new Date(ep.published).toUTCString() : new Date().toUTCString();
+      const guid = ep.id || `${Date.now()}`;
+      const audioUrl = ep?.audio?.private_mp3 || ep?.audio?.url || "";
+      const audioBytes = ep?.audio?.bytes || 0;
+      const duration = ep?.audio?.duration || 0;
+      const image = ep.image || CHANNEL.image;
+
+      return `
+        <item>
+          <title><![CDATA[${title}]]></title>
+          <description><![CDATA[${desc}]]></description>
+          <pubDate>${pub}</pubDate>
+          <guid isPermaLink="false">${guid}</guid>
+          <enclosure url="${audioUrl}" length="${audioBytes}" type="audio/mpeg" />
+          <itunes:duration>${duration}</itunes:duration>
+          <itunes:image href="${image}" />
+        </item>
+      `;
+    })
     .join("");
 
+  // ---- XML ----
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0"
-       xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+  <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
     <channel>
       <title><![CDATA[${CHANNEL.title}]]></title>
       <link>${CHANNEL.link}</link>
@@ -41,12 +52,12 @@ module.exports = async (req, res) => {
 
       <!-- Krävs av Spotify -->
       <description><![CDATA[${CHANNEL.description}]]></description>
-      <itunes:author>${CHANNEL.itunesAuthor}</itunes:author>
-      <itunes:explicit>${CHANNEL.itunesExplicit}</itunes:explicit>
-      <itunes:image href="${CHANNEL.itunesImage}"/>
+      <itunes:author><![CDATA[${CHANNEL.author}]]></itunes:author>
+      <itunes:explicit>${CHANNEL.explicit}</itunes:explicit>
+      <itunes:image href="${CHANNEL.image}"/>
       <itunes:owner>
-        <itunes:name>${CHANNEL.itunesOwnerName}</itunes:name>
-        <itunes:email>${CHANNEL.itunesOwnerEmail}</itunes:email>
+        <itunes:name>${CHANNEL.ownerName}</itunes:name>
+        <itunes:email>${CHANNEL.ownerEmail}</itunes:email>
       </itunes:owner>
 
       ${items}
@@ -54,5 +65,7 @@ module.exports = async (req, res) => {
   </rss>`;
 
   res.setHeader("Content-Type", "application/rss+xml; charset=utf-8");
+  // (valfritt) liten cache för snabbare validering/CDN
+  res.setHeader("Cache-Control", "public, max-age=0, s-maxage=60");
   res.status(200).send(xml);
 };
