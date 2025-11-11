99312995e6799062dd24222458e094dddf0529f5
tillsangs <tillsangs@gmail.com>
2025-11-11 16:35:33 +0100

    Update source.js

--- api/feed/source.js
+++ api/feed/source.js
@@ -14,6 +14,10 @@ module.exports = async (req, res) => {
     image: "https://www.tillsangs.se/cover.jpg",
     ownerName: "Samanda Ekman",
     ownerEmail: "info@tillsangs.com",
+
+    // Lägg till minst en kategori (du kan ändra dessa om du vill)
+    categoryTop: "Society & Culture",
+    categorySub: "Personal Journals",
   };
 
   // ---- Avsnitt ----
@@ -28,44 +32,11 @@ module.exports = async (req, res) => {
       const duration = ep?.audio?.duration || 0;
       const image = ep.image || CHANNEL.image;
 
+      // Skapa fallback-länk till sajt
+      const itemLink = ep.link || `${CHANNEL.link}#${encodeURIComponent(guid)}`;
+
       return `
         <item>
           <title><![CDATA[${title}]]></title>
+          <link>${itemLink}</link>
           <description><![CDATA[${desc}]]></description>
-          <pubDate>${pub}</pubDate>
-          <guid isPermaLink="false">${guid}</guid>
-          <enclosure url="${audioUrl}" length="${audioBytes}" type="audio/mpeg" />
-          <itunes:duration>${duration}</itunes:duration>
-          <itunes:image href="${image}" />
-        </item>
-      `;
-    })
-    .join("");
-
-  // ---- XML ----
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
-    <channel>
-      <title><![CDATA[${CHANNEL.title}]]></title>
-      <link>${CHANNEL.link}</link>
-      <language>${CHANNEL.language}</language>
-
-      <!-- Krävs av Spotify -->
-      <description><![CDATA[${CHANNEL.description}]]></description>
-      <itunes:author><![CDATA[${CHANNEL.author}]]></itunes:author>
-      <itunes:explicit>${CHANNEL.explicit}</itunes:explicit>
-      <itunes:image href="${CHANNEL.image}"/>
-      <itunes:owner>
-        <itunes:name>${CHANNEL.ownerName}</itunes:name>
-        <itunes:email>${CHANNEL.ownerEmail}</itunes:email>
-      </itunes:owner>
-
-      ${items}
-    </channel>
-  </rss>`;
-
-  res.setHeader("Content-Type", "application/rss+xml; charset=utf-8");
-  // (valfritt) liten cache för snabbare validering/CDN
-  res.setHeader("Cache-Control", "public, max-age=0, s-maxage=60");
-  res.status(200).send(xml);
-};
