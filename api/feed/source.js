4ef06eae85fe34d5f3f3949d9129e9ae3a762b4d
tillsangs <tillsangs@gmail.com>
2025-11-11 16:36:07 +0100

    Update source.js

--- api/feed/source.js
+++ api/feed/source.js
@@ -40,3 +40,43 @@ module.exports = async (req, res) => {
           <title><![CDATA[${title}]]></title>
           <link>${itemLink}</link>
           <description><![CDATA[${desc}]]></description>
+          <pubDate>${pub}</pubDate>
+          <guid isPermaLink="false">${guid}</guid>
+          <enclosure url="${audioUrl}" length="${audioBytes}" type="audio/mpeg" />
+          <itunes:duration>${duration}</itunes:duration>
+          <itunes:image href="${image}" />
+        </item>
+      `;
+    })
+    .join("");
+
+  // ---- XML ----
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+    <channel>
+      <title><![CDATA[${CHANNEL.title}]]></title>
+      <link>${CHANNEL.link}</link>
+      <language>${CHANNEL.language}</language>
+
+      <description><![CDATA[${CHANNEL.description}]]></description>
+      <itunes:author><![CDATA[${CHANNEL.author}]]></itunes:author>
+      <itunes:explicit>${CHANNEL.explicit}</itunes:explicit>
+      <itunes:image href="${CHANNEL.image}"/>
+      <itunes:owner>
+        <itunes:name>${CHANNEL.ownerName}</itunes:name>
+        <itunes:email>${CHANNEL.ownerEmail}</itunes:email>
+      </itunes:owner>
+
+      <!-- Kategori krävs för validering -->
+      <itunes:category text="${CHANNEL.categoryTop}">
+        <itunes:category text="${CHANNEL.categorySub}" />
+      </itunes:category>
+
+      ${items}
+    </channel>
+  </rss>`;
+
+  res.setHeader("Content-Type", "application/rss+xml; charset=utf-8");
+  res.setHeader("Cache-Control", "public, max-age=0, s-maxage=60");
+  res.status(200).send(xml);
+};
