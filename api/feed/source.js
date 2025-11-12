ba1bd0246b41fe07e5c9153f975f2de1c3acff9c
tillsangs <tillsangs@gmail.com>
2025-11-12 10:05:56 +0100

    Update source.js

--- api/feed/source.js
+++ api/feed/source.js
@@ -1,7 +1,7 @@
 // api/feed/source.js  (CommonJS)
 const episodes = require("../../content/episodes.json");
 
-// Minimal XML-escape för ATTRIBUT (t.ex. href="", url="")
+// Minimal XML-escape för attribut (href="", url="")
 const escAttr = (v) =>
   String(v ?? "")
     .replace(/&/g, "&amp;")
@@ -22,24 +22,28 @@ module.exports = async (req, res) => {
     ownerName: "Samanda Ekman",
     ownerEmail: "info@tillsangs.com",
 
-    // Kategori (OBS: & måste bli &amp;)
+    // iTunes/Spotify-kategori (OBS: & ska vara &amp;)
     categoryTop: "Society &amp; Culture",
     categorySub: "Personal Journals",
+
+    // Self-link för parser
+    selfFeedUrl: "https://www.tillsangs.se/api/feed/source",
   };
 
+  // ---- Avsnitt → RSS-items ----
   const items = (episodes || [])
     .map((ep) => {
       const title = ep.title || "Avsnitt";
       const desc = ep.descriptionHtml || "";
-      const pub = ep.published ? new Date(ep.published).toUTCString() : new Date().toUTCString();
+      const pub = ep.published
+        ? new Date(ep.published).toUTCString()
+        : new Date().toUTCString();
       const guid = ep.id || `${Date.now()}`;
 
-      const audioUrl = ep?.audio?.private_mp3 || ep?.audio?.url || "";
+      const audioUrl = ep?.audio?.private_mp3 || "";
       const audioBytes = ep?.audio?.bytes || 0;
       const duration = ep?.audio?.duration || 0;
       const image = ep.image || CHANNEL.image;
-
-      // Länk till avsnittssida; fallback = startsidan med ankare
       const itemLink = ep.link || `${CHANNEL.link}#${encodeURIComponent(guid)}`;
 
       return `
@@ -52,17 +56,25 @@ module.exports = async (req, res) => {
           <enclosure url="${escAttr(audioUrl)}" length="${audioBytes}" type="audio/mpeg" />
           <itunes:duration>${duration}</itunes:duration>
           <itunes:image href="${escAttr(image)}" />
+          <itunes:episodeType>full</itunes:episodeType>
         </item>
       `;
     })
     .join("");
 
+  // ---- RSS XML ----
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
+  <rss version="2.0"
+       xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
+       xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
       <title><![CDATA[${CHANNEL.title}]]></title>
       <link>${CHANNEL.link}</link>
       <language>${CHANNEL.language}</language>
+
+      <!-- Viktigt för vissa parser -->
+      <atom:link href="${CHANNEL.selfFeedUrl}" rel="self" type="application/rss+xml"/>
+      <itunes:type>episodic</itunes:type>
 
       <description><![CDATA[${CHANNEL.description}]]></description>
       <itunes:author><![CDATA[${CHANNEL.author}]]></itunes:author>
